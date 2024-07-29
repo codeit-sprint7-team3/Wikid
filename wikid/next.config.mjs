2177dd145ce15666a30a375dd16ddc39ee968c4a
clyde-yoon <a1056719@gmail.com>
2024-07-29 15:00:32 +0900

    ✨ 프록시 설정

--- wikid/next.config.mjs
+++ wikid/next.config.mjs
@@ -1,6 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
