a9084f840279bc610af0ee192aa1a222b7acf6ba
somang <149379670+som0301@users.noreply.github.com>
2024-08-01 06:31:17 +0900

    💄 Style: 회원가입 페이지 css 추가

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import SignUpForm from "@/components/SignUpForm";
+import style from "@/styles/signup.module.css";
+import Link from "next/link";
 
 const signUp = () => {
   return (
-    <div>
-      <h1>회원가입</h1>
+    <div className={style.signUp}>
+      <h1 className={style.header}>회원가입</h1>
       <SignUpForm />
+      <div className={style.bottom}>
+        <span>이미 회원이신가요?</span>
+        <Link href="/login">로그인하기</Link>
+      </div>
     </div>
   );
 };
