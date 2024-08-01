a6ead60d543dd15390229d421ff9f0600a914f58
clyde-yoon <a1056719@gmail.com>
2024-08-01 19:22:07 +0900

    ✨ Features : user프로필 연결

--- src/components/header/HeaderModal.tsx
+++ src/components/header/HeaderModal.tsx
@@ -1,4 +1,5 @@
 import style from '@/components/header/HeaderModal.module.css';
+import { useRouter } from 'next/router';
 
 interface HeaderModal {
   isOpen: boolean;
@@ -6,16 +7,21 @@ interface HeaderModal {
 }
 
 const HeaderModal = ({ isOpen, onClose }: HeaderModal) => {
+  const router = useRouter();
+  const navigateTowikilist = () => {
+    router.push('/wikilist');
+  };
+  const navigateToboards = () => {
+    router.push('/boards');
+  };
   if (!isOpen) return null;
 
   return (
     <div className={style.modalOverlay} onClick={onClose}>
       <div className={style.modalContent} onClick={(e) => e.stopPropagation()}>
         <ul className={style.modalUl}>
-          <li>위키목록</li>
-          <li>자유게시판</li>
-          <li>알림</li>
-          <li>마이페이지</li>
+          <li onClick={navigateTowikilist}>위키목록</li>
+          <li onClick={navigateToboards}>자유게시판</li>
         </ul>
       </div>
     </div>
--- src/store/AuthStore.ts
+++ src/store/AuthStore.ts
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';
 import api from '../lib/axios';
 import Cookies from 'js-cookie';
 
