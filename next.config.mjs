a1d46751687e1029dc66d50726b86ad5c8857d66
jiwon shin <s420204c@gmail.com>
2024-08-01 17:53:17 +0900

    💄 style: 아이콘 svg 파일 추가 및 스타일링 시도 중

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            // 추가적인 옵션을 여기에 설정할 수 있습니다.
-          },
-        },
-      ],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
