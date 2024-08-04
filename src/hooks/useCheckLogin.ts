f8fe121495d89dda5fcb837c464cde6c7b1394dc
clyde-yoon <a1056719@gmail.com>
2024-08-04 18:36:44 +0900

    ✨ Features: 로그인확인 훅에 loading 상태추가

--- src/hooks/useCheckLogin.ts
+++ src/hooks/useCheckLogin.ts
@@ -4,13 +4,15 @@ import { User } from '@/types/UserType';
 
 const useCheckLogin = () => {
   const { user } = useAuthStore();
-  const [clientUser, setClientUser] = useState<User | null>();
+  const [clientUser, setClientUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setClientUser(user);
+    setIsLoading(false);
   }, [user]);
 
-  return clientUser;
+  return { clientUser, isLoading };
 };
 
 export default useCheckLogin;
