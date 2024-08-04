420d38a411dd027c6e7d7bc5f38532cb3b3110d7
clyde-yoon <a1056719@gmail.com>
2024-08-04 23:34:50 +0900

    ♻️ Recycle : 회원가입 부분 api호출 변경

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,8 +1,8 @@
-import api from "@/lib/axios";
-import { useRouter } from "next/router";
-import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import style from "@/styles/signup.module.css";
-import classNames from "classnames";
+import basicApi from '@/lib/basicAxios';
+import { useRouter } from 'next/router';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import style from '@/styles/signup.module.css';
+import classNames from 'classnames';
 
 interface formValues {
   name: string;
@@ -14,49 +14,49 @@ interface formValues {
 const SignUpForm: React.FC = () => {
   const router = useRouter();
   const [formValue, setFormValue] = useState<formValues>({
-    name: "",
-    email: "",
-    password: "",
-    passwordConfirmation: "",
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
   });
-  const [nameError, setNameError] = useState<string>("");
-  const [emailError, setEmailError] = useState<string>("");
-  const [passwordError, setPasswordError] = useState<string>("");
+  const [nameError, setNameError] = useState<string>('');
+  const [emailError, setEmailError] = useState<string>('');
+  const [passwordError, setPasswordError] = useState<string>('');
   const [passwordConfirmationError, setPasswordConfirmationError] =
-    useState<string>("");
+    useState<string>('');
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!validateName(formValue.name)) {
-      setNameError("닉네임은 10자 내로 입력해주세요");
+      setNameError('닉네임은 10자 내로 입력해주세요');
       return;
     }
     if (!validateEmail(formValue.email)) {
-      setEmailError("잘못된 이메일 형식입니다");
+      setEmailError('잘못된 이메일 형식입니다');
       return;
     }
     if (formValue.password.length < 8) {
-      setPasswordError("비밀번호는 8자 이상이여야 합니다");
+      setPasswordError('비밀번호는 8자 이상이여야 합니다');
       return;
     }
     if (formValue.password !== formValue.passwordConfirmation) {
-      setPasswordConfirmationError("비밀번호가 일치하지 않습니다");
+      setPasswordConfirmationError('비밀번호가 일치하지 않습니다');
       return;
     }
     try {
-      await api.post("/auth/signUp", formValue);
-      router.push("/login");
+      await basicApi.post('/auth/signUp', formValue);
+      router.push('/login');
     } catch (error: any) {
       if (error.response && error.response.status === 400) {
-        if (error.response.data.message === "이미 사용중인 이메일입니다.") {
-          setEmailError("이미 사용중인 이메일입니다");
+        if (error.response.data.message === '이미 사용중인 이메일입니다.') {
+          setEmailError('이미 사용중인 이메일입니다');
         }
       } else if (error.response && error.response.status === 500) {
-        if (error.response.data.message === "Internal Server Error") {
-          setNameError("이미 사용중인 닉네임입니다");
+        if (error.response.data.message === 'Internal Server Error') {
+          setNameError('이미 사용중인 닉네임입니다');
         }
       } else {
-        console.log("회원가입에 실패했습니다");
+        console.log('회원가입에 실패했습니다');
       }
     }
   };
@@ -68,17 +68,17 @@ const SignUpForm: React.FC = () => {
       [name]: value,
     }));
     switch (name) {
-      case "name":
-        setNameError("");
+      case 'name':
+        setNameError('');
         break;
-      case "email":
-        setEmailError("");
+      case 'email':
+        setEmailError('');
         break;
-      case "password":
-        setPasswordError("");
+      case 'password':
+        setPasswordError('');
         break;
-      case "passwordConfirmation":
-        setPasswordConfirmationError("");
+      case 'passwordConfirmation':
+        setPasswordConfirmationError('');
         break;
       default:
         break;
@@ -92,7 +92,7 @@ const SignUpForm: React.FC = () => {
 
   const handleNameBlur = () => {
     if (!validateName(formValue.name)) {
-      setNameError("닉네임은 10자 내로 입력해주세요");
+      setNameError('닉네임은 10자 내로 입력해주세요');
     }
   };
 
@@ -104,21 +104,21 @@ const SignUpForm: React.FC = () => {
 
   const handleEmailBlur = () => {
     if (!validateEmail(formValue.email)) {
-      setEmailError("잘못된 이메일 형식입니다");
+      setEmailError('잘못된 이메일 형식입니다');
     }
   };
 
   //비밀번호 길이 검사
   const handlePasswordBlur = () => {
     if (formValue.password.length < 8) {
-      setPasswordError("비밀번호는 8자 이상이여야 합니다");
+      setPasswordError('비밀번호는 8자 이상이여야 합니다');
     }
   };
 
   //비밀번호 확인 검사
   const handlePasswordConfirmationBlur = () => {
     if (formValue.password !== formValue.passwordConfirmation) {
-      setPasswordConfirmationError("비밀번호가 일치하지 않습니다");
+      setPasswordConfirmationError('비밀번호가 일치하지 않습니다');
     }
   };
 
