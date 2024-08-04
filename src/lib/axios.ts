5f3936f2313d7213a6bb13be1cbe6e80ad043671
clyde-yoon <a1056719@gmail.com>
2024-08-04 22:43:48 +0900

    ✨ Features : axios interceptors 설정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
+import Cookies from 'js-cookie';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -9,5 +10,44 @@ const api: AxiosInstance = axios.create({
     Accept: 'application/json',
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response?.data?.message === 'jwt expired' &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      const refreshToken = Cookies.get('refreshToken');
+      if (refreshToken) {
+        try {
+          const response = await api.post('/auth/refresh', { refreshToken });
+          const { accessToken } = response.data;
+          Cookies.set('accessToken', accessToken);
+          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+          return api(originalRequest);
+        } catch (refreshError) {
+          console.error('토큰 갱신 실패:', refreshError);
+        }
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
