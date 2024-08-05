8491e135e096c97cada2d4e0ec8e4e7b16651049
clyde-yoon <a1056719@gmail.com>
2024-08-05 23:15:18 +0900

    ✨ Features : SearchBar 공통컴포넌트 작성

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-
+import style from '@/components/search/SearchBar.module.css';
+import Image from 'next/image';
+import glass from '@/assets/icon/type=search.svg';
 interface SearchBarProps {
   placeholder: string;
   value: string;
@@ -8,13 +9,15 @@ interface SearchBarProps {
 
 const SearchBar = ({ placeholder, value, onChange }: SearchBarProps) => {
   return (
-    <div>
+    <div className={style.inputContainer}>
       <input
         type="text"
         placeholder={placeholder}
         onChange={onChange}
         value={value}
+        className={style.searchBar}
       />
+      <Image src={glass} alt="Magnifying glass" className={style.glass} />
     </div>
   );
 };
