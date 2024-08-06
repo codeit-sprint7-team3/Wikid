e9e1c6235833651c8a46ec8fc9e35aa52d3f5070
신지원 <s420204c@gamil.com>
2024-08-06 15:21:42 +0900

    ✨ feat: 저장되는 기능 시도

---
+++ src/pages/edit.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SaveButton from '@/components/SaveButton';
+
+const EditPage: React.FC = () => {
+  return (
+    <div>
+      <h1>위키 페이지 수정</h1>
+      <SaveButton />
+    </div>
+  );
+};
+
+export default EditPage;
