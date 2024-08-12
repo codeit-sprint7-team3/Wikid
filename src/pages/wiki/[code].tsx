ad3c30881628726a9b97f670b780080d64d3e061
신지원 <s420204c@gamil.com>
2024-08-13 02:35:02 +0900

    💄 style: 스타일 변수명 수정 및 스타일 추가

--- src/pages/wiki/[code].tsx
+++ src/pages/wiki/[code].tsx
@@ -3,7 +3,7 @@ import QuizModal from '@/components/QuizModal';
 import WikiProfile from '@/components/WikiProfile';
 import useAuthStore from '@/store/AuthStore';
 import useEditmodeStore from '@/store/EditStore';
-import style from '@/styles/wiki.module.css'; // 스타일 파일을 적절히 수정합니다.
+import style from '@/styles/mywiki.module.css'; // 스타일 파일을 적절히 수정합니다.
 import { Profile } from '@/types/UserType'; // Profile 인터페이스 임포트
 import { GetServerSideProps } from 'next';
 import { useEffect, useState } from 'react';
@@ -43,55 +43,50 @@ const WikiPage = ({ profile, code }: WikiPageProps) => {
   }
 
   return (
-    <div className={style.container}>
-      <div className={style.left}>
-        <div className={style.snackbar}>
+    <div className={style.contentContainer}>
+      <div className={style.topContainer}>
+        <div className={style.subjectContainer}>
           {!isEditable && (
             <div className={style.snackbarWarning}>
-              <p>다른친구가 편집하고 있어요, 나중에 다시 시도해주세요</p>
+              <p>다른 친구가 편집하고 있어요, 나중에 다시 시도해 주세요.</p>
             </div>
           )}
-          <div>
-            <h1>{profile.name}</h1>
-            <WikiLink code={code} name='현재위키' />
-          </div>
-          {user?.id && profile.content && (
-            <button
-              className={style.startButton}
-              onClick={handleStartClick}
-              disabled={!isEditable}
-            >
-              {isEditable ? '위키 참여하기' : '편집 중ˑˑˑ'}
-            </button>
-          )}
+          <h2 className={style.h2}>{profile.name}</h2>
+          <button
+            className={style.button}
+            onClick={handleStartClick}
+            disabled={!isEditable}
+          >
+            {isEditable ? '위키 참여하기' : '편집 중ˑˑˑ'}
+          </button>
         </div>
-        <div className={style.wikiContent}>
-          {profile.content ? (
-            <div className={style.nonDataWrap}>
-              <div
-                dangerouslySetInnerHTML={{ __html: profile.content || '' }}
-              />
-            </div>
-          ) : (
-            <div className={style.nonDataWrap}>
-              <p>아직 작성된 내용이 없네요. 위키에 참여해보세요!</p>
-              <button
-                className={style.startButton}
-                onClick={handleStartClick}
-                disabled={!isEditable}
-              >
-                시작하기
-              </button>
-            </div>
-          )}
+        <div className={style.link}>
+          <WikiLink name={profile.name} code={code} />
         </div>
-
-        <QuizModal isOpen={isQuizOpen} onClose={() => setIsQuizOpen(false)} />
       </div>
-
-      <div className={style.profileContainer + style.right}>
+      <div className={style.profileContainer}>
         <WikiProfile profile={profile} />
       </div>
+      {profile.content ? (
+        <div className={style.description}>
+          <span
+            dangerouslySetInnerHTML={{ __html: profile.content || '' }}
+          ></span>
+        </div>
+      ) : (
+        <div className={style.nonDataWrap}>
+          <p>아직 작성된 내용이 없네요.</p>
+          <p>위키에 참여해 보세요!</p>
+          <button
+            className={style.button}
+            onClick={handleStartClick}
+            disabled={!isEditable}
+          >
+            시작하기
+          </button>
+        </div>
+      )}
+      <QuizModal isOpen={isQuizOpen} onClose={() => setIsQuizOpen(false)} />
     </div>
   );
 };
